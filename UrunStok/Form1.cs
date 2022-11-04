c99ed57295ce13a476284f15763eebaad34019ba
AtahanCelebi <atahancelebi98@gmail.com>
2022-11-04 22:30:45 +0300

    userLogin check

--- UrunStok/Form1.cs
+++ UrunStok/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -31,8 +32,44 @@ namespace UrunStok
 
         private void button1_Click(object sender, EventArgs e)
         {
-            homePage2 frm = new homePage2();
-            frm.Show();
+            if (checkIfExsist(txtUserName.Text, txtPassword.Text))
+            {
+                homePage2 frm = new homePage2();
+                frm.Show();
+
+            }
+            else
+            {
+                
+                MessageBox.Show("Bilgileriniz Yanlış", "Girmiş olduğunuz bilgiler yanlış\nya da mevcut değil",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+        public bool checkIfExsist(string userName, string password)
+        {
+
+            SqlConnection conn1 = new SqlConnection("Server=DESKTOP-LKOHK1C;Database=stok;Trusted_Connection=True;");
+
+            conn1.Open();
+            SqlCommand cmd1 = new SqlCommand("SELECT userName,user_password FROM userInfo WHERE userName=@nName OR user_password=@password", conn1);
+            cmd1.Parameters.AddWithValue("@nName", userName);
+            cmd1.Parameters.AddWithValue("@password", password);
+            SqlDataReader readar1;
+            readar1 = cmd1.ExecuteReader();
+            if (readar1.Read())
+            {
+                if (userName == readar1["userName"].ToString() && password == readar1["user_password"].ToString())
+                {
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
